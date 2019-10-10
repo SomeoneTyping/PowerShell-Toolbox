d3cf829bab2dc55022f45cd29d4f3eddd206adee
someone_typing <konrad@daleske.de>
2019-10-10 13:41:37 +0200

    unifiy jump -remove command

--- Module_Jumper/Public/Enter-Folder.ps1
+++ Module_Jumper/Public/Enter-Folder.ps1
@@ -15,7 +15,7 @@ function Enter-Folder {
         [switch]
         $List,
 
-        [switch]
+        [string]
         $Remove
     )
 
@@ -38,6 +38,8 @@ function Enter-Folder {
                 $newPathObject = New-PathObject -Tag $Add -Path $currentLocation
                 Add-CsvValue -Path $pathesFile -PsObject $newPathObject
             }
+
+            return
         }
 
         $resultsForContainsSearch = Get-CsvValues -Path $pathesFile -Key "tag" -ValueContains $tagValidated
@@ -46,12 +48,12 @@ function Enter-Folder {
             return
         }
 
-        if ($Remove.IsPresent) {
-            $resultsForContainsSearch | ForEach-Object { $color = if (Test-Path $_.path) { "White" } else { "Yellow" }; Write-InfoLine $_.tag $_.path -space 18 -color $color }
-            $resultCount = ($resultsForContainsSearch | Measure-Object).Count
+        if ($Remove) {
+            $searchRemoveRequest = Get-CsvValues -Path $pathesFile -Key "tag" -Value $Remove
+            $resultCount = ($searchRemoveRequest | Measure-Object).Count
             $input = Read-Host ([string]::Format("Remove these {0} entries? [J/N]", $resultCount))
             if ($input -eq "j") {
-                foreach($entry in $resultsForContainsSearch) {
+                foreach($entry in $searchRemoveRequest) {
                     Remove-CsvValue -Path $pathesFile -Key "tag" -Value $entry.tag
                 }
             }
